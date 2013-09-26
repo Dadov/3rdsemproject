c4974486cbf072131de6780f110a10d8ff71b8d3
tomiberes <tomiberes@gmail.com>
2013-09-26 08:12:53 +0000

    Entity Framework generated files

---
+++ ElectricCarGroup8/ElectricCarDB/ElectricCarModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\tomi\School\3rd semester\VS Projects\3rdSemProject\ElectricCarGroup8\ElectricCarGroup8\ElectricCarDB\ElectricCarModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
