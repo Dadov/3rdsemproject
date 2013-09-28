f56b8823c0065389db2e46dd4933205e505363c0
david.vannay@gmail.com <david.vannay@gmail.com@9f5fed07-e13f-d60d-a418-9bd12a09d76f>
2013-09-28 19:59:17 +0000

    Battery and Battery Type basic methods Test to be done

---
+++ trunk/ElectricCarGroup8/ElectricCarLib/BatteryCtr.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectricCarDB;
+using System.Transactions;
+using System.Data;
+using System.Data.Objects;
+using System.Data.Entity;
+
+namespace ElectricCarLib
+{
+    class BatteryCtr : IDisposable
+    {
+
+        public void addNewRecord(int id, string state, int btid)
+        {
+            IDBBattery dbBattery = new DBBattery();
+            dbBattery.addNewRecord(id, state, btid);
+
+        }
+
+        public MBattery getRecord(int id, Boolean getAssociation)
+        {
+            IDBBattery dbBattery = new DBBattery();
+            return dbBattery.getRecord(id, true);
+        }
+
+        public void deleteRecord(int id)
+        {
+            IDBBattery dbBattery = new DBBattery();
+            dbBattery.deleteRecord(id);
+        }
+
+        public void updateRecord(int id, string state, int btid)
+        {
+            IDBBattery dbBattery = new DBBattery();
+            dbBattery.updateRecord(id, state, btid);
+        }
+
+        public List<MBattery> getAllRecord(Boolean getAssociation)
+        {
+            IDBBattery dbBattery = new DBBattery();
+            return dbBattery.getAllRecord(true);
+        }
+
+        public List<string> getAllInfo()
+        {
+            IDBBattery dbBattery = new DBBattery();
+            return dbBattery.getAllInfo();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            this.Dispose();
+        }
+    }
+}
---
+++ trunk/ElectricCarGroup8/ElectricCarLib/BatteryTypeCtr.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectricCarDB;
+using System.Transactions;
+using System.Data;
+using System.Data.Objects;
+using System.Data.Entity;
+
+namespace ElectricCarLib
+{
+    class BatteryTypeCtr:IDisposable
+    {
+
+        public void addNewRecord(int id, string name, string producer, decimal capacity, decimal exchangeCost, int storageNumber)
+        {
+            IDBBatteryType dbBatteryType = new DBBatteryType();
+            dbBatteryType.addNewRecord(id, name, producer, capacity, exchangeCost, storageNumber);
+
+        }
+
+        public MBatteryType getRecord(int id, Boolean getAssociation)
+        {
+            IDBBatteryType dbBatteryType = new DBBatteryType();
+            return dbBatteryType.getRecord(id, true);
+        }
+
+        public void deleteRecord(int id)
+        {
+            IDBBatteryType dbBatteryType = new DBBatteryType();
+            dbBatteryType.deleteRecord(id);
+        }
+
+        public void updateRecord(int id, string name, string producer, decimal capacity, decimal exchangeCost, int storageNumber)
+        {
+            IDBBatteryType dbBatteryType = new DBBatteryType();
+            dbBatteryType.updateRecord(id, name, producer, capacity, exchangeCost, storageNumber);
+        }
+
+        public List<MBatteryType> getAllRecord(Boolean getAssociation)
+        {
+            IDBBatteryType dbBatteryType = new DBBatteryType();
+            return dbBatteryType.getAllRecord(true); 
+        }
+
+        public List<string> getAllInfo()
+        {
+            IDBBatteryType dbBatteryType = new DBBatteryType();
+            return dbBatteryType.getAllInfo();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            this.Dispose();
+        }
+    }
+}
