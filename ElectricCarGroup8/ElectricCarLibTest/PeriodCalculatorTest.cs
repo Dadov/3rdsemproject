6d5c09fddd29a3e75e8f3ce485355df104711d37
david.vannay <david.vannay@gmail.com>
2013-10-14 10:45:32 +0000

    fixed test

--- ElectricCarGroup8/ElectricCarLibTest/PeriodCalculatorTest.cs
+++ ElectricCarGroup8/ElectricCarLibTest/PeriodCalculatorTest.cs
@@ -19,34 +19,77 @@ namespace ElectricCarLibTest
 
         public PeriodCalculatorTest()
         {
-            int btId = dbType.addNewRecord("newName", "newProducer", 10, 100, 20); //capacity equals 10 hours
-            int sID = dbStation.addNewRecord("newName", "newAddress", "newCountry", "newState");
-            int bsID = dbStorage.addNewRecord(btId,sID);
-            dbPeriod.addNewRecord(bsID, DateTime.Today,10,5,1); // initial =10 custom = 5 future = 1
-            period = dbPeriod.getRecord(bsID, DateTime.Today, true);
-            storage = dbStorage.getRecord(bsID, true);
+            
         }
         [TestMethod]
         public void TestGetTime()
         {
-            DateTime newTime = pCalc.getTime(storage);
-            Assert.AreEqual(newTime, DateTime.Today.AddHours(10));
-        }
+            int btId = dbType.addNewRecord("newName", "newProducer", 10, 100, 20); //capacity equals 10 hours
+            int sID = dbStation.addNewRecord("newName", "newAddress", "newCountry", "newState");
+            int bsID = dbStorage.addNewRecord(btId, sID);
+            dbPeriod.addNewRecord(bsID, DateTime.Today, 10, 5, 1); // initial =10 custom = 5 future = 1
+            period = dbPeriod.getRecord(bsID, DateTime.Today, true);
+            storage = dbStorage.getRecord(bsID, true);
+            try
+            {
+                DateTime newTime = pCalc.getTime(storage);
+                Assert.AreEqual(newTime, DateTime.Today.AddHours(10));
+            }
+            finally
+            {
+                dbPeriod.deleteRecord(bsID, DateTime.Today);
+                dbStorage.deleteRecord(bsID);
+                dbStation.deleteRecord(sID);
+                dbType.deleteRecord(btId);
+            }
+            }
         [TestMethod]
         public void TestGetInitNumber()
         {
-            int init = pCalc.getInitNumber(storage);
-            Assert.AreEqual(init, 5);
+            int btId = dbType.addNewRecord("newName", "newProducer", 10, 100, 20); //capacity equals 10 hours
+            int sID = dbStation.addNewRecord("newName", "newAddress", "newCountry", "newState");
+            int bsID = dbStorage.addNewRecord(btId, sID);
+            dbPeriod.addNewRecord(bsID, DateTime.Today, 10, 5, 1); // initial =10 custom = 5 future = 1
+            period = dbPeriod.getRecord(bsID, DateTime.Today, true);
+            storage = dbStorage.getRecord(bsID, true);
+            try
+            {
+                int init = pCalc.getInitNumber(storage);
+                Assert.AreEqual(init, 5);
+            }
+            finally
+            {
+                dbPeriod.deleteRecord(bsID, DateTime.Today);
+                dbStorage.deleteRecord(bsID);
+                dbStation.deleteRecord(sID);
+                dbType.deleteRecord(btId);
+            }
         }
         [TestMethod]
         public void TestGetPeriod()
         {
-            MPeriod firstPeriod = pCalc.getPeriod(storage);
-            storage = dbStorage.getRecord(storage.id, true);
-            MPeriod secondPeriod = pCalc.getPeriod(storage);
-            Assert.AreEqual(DateTime.Today.AddHours(20), secondPeriod.time);
-            Assert.AreEqual(9, secondPeriod.initBatteryNumber);
-            Assert.AreEqual(1, firstPeriod.bookedBatteryNumber);
+            int btId = dbType.addNewRecord("newName", "newProducer", 10, 100, 20); //capacity equals 10 hours
+            int sID = dbStation.addNewRecord("newName", "newAddress", "newCountry", "newState");
+            int bsID = dbStorage.addNewRecord(btId, sID);
+            dbPeriod.addNewRecord(bsID, DateTime.Today, 10, 5, 1); // initial =10 custom = 5 future = 1
+            period = dbPeriod.getRecord(bsID, DateTime.Today, true);
+            storage = dbStorage.getRecord(bsID, true);
+            try
+            {
+                MPeriod firstPeriod = pCalc.getPeriod(storage);
+                storage = dbStorage.getRecord(storage.id, true);
+                MPeriod secondPeriod = pCalc.getPeriod(storage);
+                Assert.AreEqual(DateTime.Today.AddHours(20), secondPeriod.time);
+                Assert.AreEqual(9, secondPeriod.initBatteryNumber);
+                Assert.AreEqual(1, firstPeriod.bookedBatteryNumber);
+            }
+            finally
+            {
+                dbPeriod.deleteRecord(bsID, DateTime.Today);
+                dbStorage.deleteRecord(bsID);
+                dbStation.deleteRecord(sID);
+                dbType.deleteRecord(btId);
+            }
         }
     }
 }
