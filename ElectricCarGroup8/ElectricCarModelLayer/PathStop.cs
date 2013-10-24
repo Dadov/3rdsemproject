87b43ad381a9a47b640e76656b51ea48e20acb34
yaxuanhe <yaxuanhe@gmail.com>
2013-10-24 18:05:03 +0000

    Algorithm added without test

---
+++ ElectricCarGroup8/ElectricCarModelLayer/PathStop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricCarModelLayer
+{
+    public class PathStop
+    {
+        public int stationID { get; set; }
+        public decimal driveHour { get; set; }
+        public MStation station { get; set; }
+        
+    }
+}
