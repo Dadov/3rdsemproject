66b7da0bceed4b321fb828ed1208b034c7841ab2
tomiberes@gmail.com <tomiberes@gmail.com@9f5fed07-e13f-d60d-a418-9bd12a09d76f>
2013-11-16 14:13:15 +0000

    people WCF skeleton (wip)

---
+++ trunk/ElectricCarGroup8/ElectricCarWCF/LogInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ElectricCarWCF
+{
+    [DataContract]
+    public class LogInfo
+    {
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string LoginName { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
